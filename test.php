a9136f468e15d8a1e9c7d32ae0c9b8f378736216
velvet_vixen <hei.karenandriantasy@gmail.com>
2026-02-05 13:49:03 +0400

    feat : list

--- test.php
+++ test.php
@@ -6,3 +6,15 @@ $users = $stmt->fetchAll();
 
 echo "<pre>";
 print_r($users);
+
+
+// remplacer student par courses 
+// ajouter appointement dans le rubrique
+// integrer careers dabs contact us
+// supprimer le logo dans le navbar
+// amincir le header 
+// ajouter la barre de recherche dans main section 
+// deployer le logo cheval en ligne 
+// ajouter un titre pour le chatbox 
+
+
