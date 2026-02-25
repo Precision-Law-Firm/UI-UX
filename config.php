859d707b1c5a492cd8118ea9a185170c53b3c9dd
velvet_vixen <hei.karenandriantasy@gmail.com>
2026-02-25 14:14:25 +0400

    chore : db connection

--- config.php
+++ config.php
@@ -1,26 +1,21 @@
 <?php
-// config.php - Connexion à la base Alwaysdata
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Content-Type');
-header('Access-Control-Allow-Methods: POST');
-
-// Infos Alwaysdata
-define('DB_HOST', 'mysql-precisionlawfirm.alwaysdata.net');  
-define('DB_NAME', 'precisionlawfirm_db');                    
-define('DB_USER', 'precisionlawfirm');                     
-define('DB_PASS', 'pomVet-xohje5-kanniq');                  
+$host =  "srv502.hstgr.io";
+$port = 3306;
+$dbname = "u662658788_plf_db";
+$username = "u662658788_plf";
+$password = "y>Mf9|v$9X/";
 
 try {
-    $pdo = new PDO(
-        "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8mb4",
-        DB_USER,
-        DB_PASS,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]
-    );
+ $pdo = new PDO(
+ "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+ $username,
+ $password,
+ [
+ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+ PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+ PDO::ATTR_EMULATE_PREPARES => false,
+ ]
+ );
 } catch (PDOException $e) {
-    die("Erreur de connexion à la base : " . $e->getMessage());
+ die("DB error: " . $e->getMessage());
 }
-?>
